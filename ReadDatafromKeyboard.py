17e87dc7fbdbe20f4a4af3ed899dd12fc8092b23
MyWorld <arunp.arunp15@gamil.com>
2018-08-18 23:23:04 +0530

    Second commit. Here we are showing how we can accept input from keyboard and printing it. using input method we can accept any type of value from keyboard. this is implemented in this commit

---
+++ ReadDatafromKeyboard.py
@@ -0,0 +1,7 @@
+name=input("What is your name")
+age=input("how old are you")
+pi=input("value of pi in circle")
+
+print(name,'\n',age,'\n',pi)
+
+#here we are accepting value from keyboard and printing it.
