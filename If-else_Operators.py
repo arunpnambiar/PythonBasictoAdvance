2199bcc25dda36370275d7bb898e08794968d702
MyWorld <arunp.arunp15@gamil.com>
2018-08-19 22:42:49 +0530

    showing how to implement if-else condition checking in python

---
+++ If-else_Operators.py
@@ -0,0 +1,11 @@
+a=10
+if(a>=10):
+    print ("a is greater than 10 or equals")
+else:
+    print ("a is less than 10")
+
+a=1
+if(a>=10):
+    print ("a is greater than 10 or equals")
+else:
+    print ("a is less than 10")
